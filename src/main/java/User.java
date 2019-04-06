6988b5ebbd881b11a92dd58bdda346544eff07c6
Willicornio <guillecacerola@gmail.com>
2019-04-06 14:31:23 +0200

    meh

---
+++ src/main/java/User.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+
+public class User {
+    String nombre;
+    String surname;
+    String idUser;
+    LinkedList<Bike> listaBike;
+
+    public User (String idUser,String nombre, String surname){
+        this.nombre = nombre;
+        this.surname = surname;
+        this.idUser = idUser;
+        this.listaBike = new LinkedList<Bike>();
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSurname(){
+        return nombre;
+    }
+
+    public void setSurname(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void addBike ( Bike b){
+        this.listaBike.add(b);
+    }
+    public LinkedList<Bike> consultaBike(){
+
+        return this.listaBike;
+
+    }
+}
